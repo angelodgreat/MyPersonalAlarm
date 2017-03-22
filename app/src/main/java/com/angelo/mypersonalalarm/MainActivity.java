d64c92ff2f7757a5dcd4b92f524b5ec0cc563c38
angelodgreat <carions46@gmail.com>
2017-03-22 12:13:30 +0800

    Added Interface and Settings to accommodate gaming mode plan Updated to Latest gradle.

--- app/src/main/java/com/angelo/mypersonalalarm/MainActivity.java
+++ app/src/main/java/com/angelo/mypersonalalarm/MainActivity.java
@@ -1,13 +1,62 @@
 package com.angelo.mypersonalalarm;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_settings)Button btn_settigns;
+    @BindView(R.id.btn_exit)Button btn_exit;
+    @BindView(R.id.btn_setalarm)Button btn_setalarm;
+    @BindView(R.id.cb_alarmeveryhour)CheckBox cb_alarmeveryhour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        btn_settigns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Settings.class));
+            }
+        });
+
+        btn_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showexitprompt();
+            }
+        });
+
+
+
+    }
+    public void showexitprompt() {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == DialogInterface.BUTTON_POSITIVE) {
+                    finish();
+                    System.exit(0);
+                } else {
+
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener).show();
     }
 }
